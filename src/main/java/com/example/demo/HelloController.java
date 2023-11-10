c5920806b592c26726949f4bf3d9e11928a3094a
danbab <danbab2@naver.com>
2023-11-10 17:19:50 +0900

    d1123ㅕㅑ

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping
     public String hello(){
-        return "자흦ㅎㅈ헺헤즣ㅈㅎwhy Hello jenkins!!!!!!!!";
+        return "Hello jenkins!!!!!!!!";
     }
 
 }
